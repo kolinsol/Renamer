f265b2afbece269eb5769509a5aa8033873514b9
Yauheni Tsiarokhin <hihihiko00@gmail.com>
2017-03-20 00:44:34 +0300

    recursive delete, rename and print

--- src/Renamer.java
+++ src/Renamer.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.util.Scanner;
 
 import static java.lang.System.in;
-import static java.lang.System.setOut;
 
 /**
  * Created by kolinsol on 2/26/17.
@@ -22,7 +21,11 @@ class Renamer
     void run() {
         Scanner input = new Scanner(in);
         int index = 0;
-        deleteInvisibleFiles();
+        try {
+            deleteInvisibleFiles();
+        } catch (EmptyFolderExceprtion e) {
+            System.out.println("Empty directory");
+        }
         while(true) {
             System.out.println("1. Show list of files.\n" +
                     "2. Rename files.\n" +
@@ -34,15 +37,15 @@ class Renamer
                         printFileNames();
                     } catch (EmptyFolderExceprtion e) {
                     System.out.println("Empty directory");
-                    return;
+                    break;
                     }
                     break;
                 case 2:
                     try {
-                        printFileNames();
+                        createFilesList();
                     } catch (EmptyFolderExceprtion e) {
                         System.out.println("Empty directory");
-                        return;
+                        break;
                     }
                     recursiveRenameFiles();
                     break;
@@ -52,8 +55,8 @@ class Renamer
         }
     }
 
-    private void deleteInvisibleFiles() {
-        File[] files = folder.listFiles();
+    private void deleteInvisibleFiles() throws EmptyFolderExceprtion {
+        File[] files = createFilesList();
         for (File file : files) {
             if (file.getName().charAt(0)=='.'){
                 if (!file.delete())
@@ -63,13 +66,15 @@ class Renamer
     }
 
     private void printFileNames() throws EmptyFolderExceprtion {
-        File[] child_files = createFilesList();
-        for (File child: child_files)
-            System.out.println(child.getName());
-    }
-
-    int folderSize () {
-        return folder.listFiles().length;
+        File[] files = createFilesList();
+        for (File file: files) {
+            if (file.isDirectory()) {
+                System.out.println("\nDirectory: "+file.getAbsolutePath());
+                new Renamer(file.getAbsolutePath()).printFileNames();
+                System.out.println("\n");
+            } else
+                System.out.println(file.getName());
+        }
     }
 
     private String[] formatFileNamesByTemplate() {
@@ -78,48 +83,41 @@ class Renamer
         for (int i=0; i<file_names.length;i++)
         {
             file_names[i] = files[i].getName();
-            file_names[i] = file_names[i].replace(".mp3","");
-            file_names[i] = file_names[i].replace("[www.MP3Fiber.com]","");
-            file_names[i] = file_names[i].replace("[MP3Fiber.com]","");
-            file_names[i] = file_names[i].replace("_", " ");
-            file_names[i] = file_names[i].toLowerCase();
-            file_names[i] = file_names[i].replace("produced by", " (p ");
-            file_names[i] = file_names[i].replace("production by", " (p ");
-            file_names[i] = file_names[i].replace(" production ", " (p ");
-            file_names[i] = file_names[i].replace("prod by", " (p ");
-            file_names[i] = file_names[i].replace(" prod ", " (p ");
-            file_names[i] = file_names[i].replace("p by", " (p ");
-            file_names[i] = file_names[i].replace("featuring", " (w ");
-            file_names[i] = file_names[i].replace(" feat ", " (w ");
-            file_names[i] = file_names[i].replace(" ft ", " (w ");
-            file_names[i] = file_names[i].replaceAll("^ +| +$|( )+", "$1");
-            if(file_names[i].contains("(p"))
-                file_names[i]=file_names[i]+")";
-            if((file_names[i].contains("(w")) && !(file_names[i].contains("(p")))
-                file_names[i]=file_names[i]+")";
-            if((file_names[i].contains("(w")) && (file_names[i].contains("(p")))
-                file_names[i] = file_names[i].replace(" (p", ") (p");
-            file_names[i] = file_names[i]+".mp3";
+            if ((!files[i].isDirectory()) && file_names[i].contains(".mp3")) {
+                file_names[i] = file_names[i].replace(".mp3", "");
+                file_names[i] = file_names[i].replace("[www.MP3Fiber.com]", "");
+                file_names[i] = file_names[i].replace("[MP3Fiber.com]", "");
+                file_names[i] = file_names[i].replace("_", " ");
+                file_names[i] = file_names[i].toLowerCase();
+                file_names[i] = file_names[i].replace("produced by", " (p ");
+                file_names[i] = file_names[i].replace("production by", " (p ");
+                file_names[i] = file_names[i].replace(" production ", " (p ");
+                file_names[i] = file_names[i].replace("prod by", " (p ");
+                file_names[i] = file_names[i].replace(" prod ", " (p ");
+                file_names[i] = file_names[i].replace("p by", " (p ");
+                file_names[i] = file_names[i].replace("featuring", " (w ");
+                file_names[i] = file_names[i].replace(" feat ", " (w ");
+                file_names[i] = file_names[i].replace(" ft ", " (w ");
+                file_names[i] = file_names[i].replaceAll("^ +| +$|( )+", "$1");
+                if (file_names[i].contains("(p"))
+                    file_names[i] = file_names[i] + ")";
+                if ((file_names[i].contains("(w")) && !(file_names[i].contains("(p")))
+                    file_names[i] = file_names[i] + ")";
+                if ((file_names[i].contains("(w")) && (file_names[i].contains("(p")))
+                    file_names[i] = file_names[i].replace(" (p", ") (p");
+                file_names[i] = file_names[i] + ".mp3";
+            }
         }
         return(file_names);
     }
 
     private void renameFiles() {
-        int renameCounter = 0;
         File[] files = folder.listFiles();
         String[] file_names = formatFileNamesByTemplate();
-        if(files.length==file_names.length) {
-            for(int i=0; i<files.length;i++)
-                if(files[i].renameTo(new File(folder_name+"/"+file_names[i]))) {
-                    renameCounter++;
-                }
-                else
-                    System.out.println("file ["+(i+1)+"] not renamed.");
+        for(int i=0; i<files.length;i++) {
+            if(!files[i].renameTo(new File(folder_name+"/"+file_names[i])))
+                System.out.println("Error renaming file "+files[i].getName());
         }
-        else
-            System.out.println("wrong number of files.");
-        if (renameCounter==folderSize())
-            System.out.println("All files renamed successfully");
     }
 
     private void recursiveRenameFiles() {
